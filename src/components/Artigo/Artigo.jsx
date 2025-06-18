ea029c8b732ccea567f2a4847fce5a890062a32e
JCM20244 <jcumbe.info@gmail.com>
2025-06-18 04:02:07 +0200

    Update Artigo.jsx

--- src/components/Artigo/Artigo.jsx
+++ src/components/Artigo/Artigo.jsx
@@ -59,7 +59,7 @@ export default function Artigo() {
         return <strong className='text-danger'>EXPIRADO</strong>;
       }
     }
-if(token){
+if(!token){
   return (
     <main className='mx-0' style={{backgroundColor: '#e1e8f7', color: '#222'}}>
         <div className='row mx-0'>
